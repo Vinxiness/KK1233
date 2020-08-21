13c6552bc913f3a732a1c60eddd84e71b6bb25a9
Yulong <Yulong@qq.com>
2020-08-21 17:51:40 +0800

    asdadsa

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+
+
+
+public class Hello {
+    public static void main(String[] args) {
+        Hi sc = new Hi();
+        sc.setAge(3);
+        sc.setName("YulongHu");
+        System.out.println(sc.getAge() + sc.getName());
+        System.out.println(sc.toString());
+    }
+}
